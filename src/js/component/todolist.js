3d85fe1812cac76eeeb2ac9643272de9e636dfef
Antonio Josué Gaitán Quesada <josue.qu12@gmail.com>
2021-03-09 02:24:57 +0000

    Fetch done

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -9,14 +9,41 @@ export function Todolist() {
 	const [lenItems, setlenItems] = useState("");
 	const [items, setItems] = useState([]);
 
+	//CARGA INICIAL
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/quesada12")
+			.then(response => response.json())
+			.then(result => setItems(result))
+			.catch(error => console.log("error", error));
+	}, []);
+
+	//ACTUALIZACION
 	useEffect(() => {
 		setlenItems(items.length);
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/quesada12", {
+			method: "PUT",
+			body: JSON.stringify(items),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	});
 
 	//Events
 	const addItem = e => {
 		if (e.key == "Enter" && inputValue != "") {
-			setItems(prevState => [...prevState, inputValue]);
+			let item = { label: inputValue, done: false };
+			setItems(prevState => [...prevState, item]);
 			setInputValue("");
 		}
 	};
@@ -29,11 +56,46 @@ export function Todolist() {
 		);
 	};
 
+	const deleteList = e => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/quesada12", {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.text())
+			.then(result => {
+				console.log(result);
+				fetch(
+					"https://assets.breatheco.de/apis/fake/todos/user/quesada12",
+					{
+						method: "POST",
+						body: "[]",
+						headers: {
+							"Content-Type": "application/json"
+						}
+					}
+				)
+					.then(response => response.text())
+					.then(result => {
+						console.log(result);
+						fetch(
+							"https://assets.breatheco.de/apis/fake/todos/user/quesada12"
+						)
+							.then(response => response.json())
+							.then(result => setItems(result))
+							.catch(error => console.log("error", error));
+					})
+					.catch(error => console.log("error", error));
+			})
+			.catch(error => console.log("error", error));
+	};
+
 	// Structure
 	let itemsMap = items.map((element, index) => {
 		return (
 			<Task
-				element={element}
+				element={element.label}
 				key={index}
 				function={e => deleteItem(index)}
 			/>
@@ -43,6 +105,7 @@ export function Todolist() {
 	return (
 		<div className="mt-3">
 			<h1 className="text-center display-4 text-primary">To Do List</h1>
+
 			<div className="listContainer">
 				<input
 					className="list-group-item"
@@ -54,8 +117,14 @@ export function Todolist() {
 					placeholder="Write task"></input>
 				<ul className="list-group">
 					{itemsMap}
-					<li className="list-group-item">
+					<li className="list-group-item item">
 						<strong>{lenItems} items left</strong>
+						<button
+							type="button"
+							className="btn btn-danger"
+							onClick={e => deleteList(e)}>
+							Eliminar Lista
+						</button>
 					</li>
 				</ul>
 			</div>
